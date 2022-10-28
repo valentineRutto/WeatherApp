e2b4e2a87284a9572c41210dce619ed1d9eab72c
valentinerutto <rutto28@gmail.com>
2022-10-28 10:21:53 +0300

    -add primany key

--- app/src/main/java/com/valentinerutto/weatherapp/data/local/entities/CurrentWeather.kt
+++ app/src/main/java/com/valentinerutto/weatherapp/data/local/entities/CurrentWeather.kt
@@ -1,6 +1,10 @@
 package com.valentinerutto.weatherapp.data.local.entities
 
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 @Entity
-data class CurrentWeather(val id: Int)
+data class CurrentWeather(
+    @PrimaryKey
+    val id: Int
+    )
